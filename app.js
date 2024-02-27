6d7fcaea3705329b18749341841b9ebda93648a2
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-02-27 12:27:06 +0530

    created custom error handler

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
-
-
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const port = 3000;
 
+// server
+app.use("/api/v1/tasks", tasks);
 
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
 
-// app.use(notFound);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-// server
-app.use("/api/v1/tasks", tasks);
 
 // app.get("/api/v1/tasks")  // get all the tasks
 // app.post("/api/v1/tasks")  // create a new task
